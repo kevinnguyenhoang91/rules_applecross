838f5c460cf406a39f246c3115592b8bf0c8eec7
KhaNH3 <khanh3@onemount.com>
2021-06-18 22:24:25 +0700

    wip

--- tools/set-developer-dir.sh
+++ tools/set-developer-dir.sh
@@ -3,8 +3,8 @@
 # This is a hack -- many Apple rules don't include tools in their action
 # inputs, but rely on xcrun to invoke tools.
 
-set -euo pipefail
-
-bazel fetch //tests/data:dummy_lib
-DEVELOPER_DIR="$(bazel info output_base)/external/apple_cross_toolchain/Xcode.app/Contents/Developer"
-sudo xcode-select -s "$DEVELOPER_DIR"
+set -uo pipefail
+sudo /usr/bin/xcode-select -s "$HOME"
+tools/bazeliskw fetch @rules_applecross//tests/data:dummy_lib
+DEVELOPER_DIR="$(tools/bazeliskw info output_base)/external/apple_cross_toolchain/Xcode.app/Contents/Developer"
+sudo /usr/bin/xcode-select -s "$DEVELOPER_DIR"
